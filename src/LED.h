7658ac162153bb45c328e504fba24184affc8e3f
GIOVANNA MAFRA ZAU <57087573888@info.local>
2026-04-14 08:55:24 -0300

    primeiro commit

---
+++ src/LED.h
@@ -0,0 +1,49 @@
+#ifndef LED_h
+#define LED_h
+
+#include <Arduino.h>
+
+
+class Led
+{
+   private:
+      uint8_t pino; 
+      bool estado; //atributo
+      bool desligarPorTempo = false;
+      uint32_t desligarNoMomento;
+
+      uint32_t tempoAnteriorPiscar;
+      uint32_t tempoEspera;
+      bool estadoPiscar;
+      uint16_t repeticoes;
+
+
+      void funcaoPiscar();
+      void funcaoDesligamento();
+
+   public:
+      Led(uint8_t pin); //Método Construtor, deverá ter o mesmo nome da Classe
+
+      void ligar(); //OK 
+      void ligar(uint32_t tempoLigado_ms);
+
+      void desligar(); //OK
+
+      void piscar();
+      void piscar(float frequencia);
+      void piscar(uint8_t frequencia, uint16_t repeticoes);
+
+       bool getEstado();
+      uint8_t getPino();
+
+      void setEstado(bool estado);
+
+      void alternar();
+
+      void update();
+
+
+};
+
+
+#endif
